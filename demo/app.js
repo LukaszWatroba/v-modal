a0ae70e1cfd9f871307ad0d00b0a8d67dc943643
LukaszWatroba <lukasz.jan.watroba@gmail.com>
2015-05-01 15:27:52 +0200

    v1.3.0

--- demo/app.js
+++ demo/app.js
@@ -40,15 +40,7 @@
     .controller('LoginController', function ($scope, loginModal) {
       var ctrl = this;
 
-      $scope.model = {};
-
       ctrl.close = loginModal.deactivate;
-    })
-
-    .controller('InfoController', function ($scope, infoModal) {
-      var ctrl = this;
-
-      ctrl.close = infoModal.deactivate;
     })
 
     .controller('LoremController', function ($scope, loremModal) {
@@ -57,13 +49,28 @@
       ctrl.close = loremModal.deactivate;
     })
 
+    .controller('InfoController', function ($scope, infoModal) {
+      var ctrl = this;
+
+      ctrl.close = infoModal.deactivate;
+    })
+
 
     .controller('MainController', function (loginModal, infoModal, loremModal) {
       var ctrl = this;
 
+      var lorem = {
+        title: 'Lorem ipsum dolor',
+        content: 'Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida vitae, dapibus risus ante sodales lectus blandit eu, tempor diam pede cursus vitae, ultricies eu, faucibus quis, porttitor eros cursus lectus, pellentesque eget, bibendum a, gravida ullamcorper quam. Nullam viverra consectetuer. Quisque cursus et, porttitor risus. Aliquam sem. In hendrerit nulla quam nunc, accumsan congue.'
+      };
+
       ctrl.openLoginModal = loginModal.activate;
       ctrl.openInfoModal = infoModal.activate;
-      ctrl.openLoremModal = loremModal.activate;
+
+      // Add params to the modal $scope
+      ctrl.openLoremModal = function () {
+        loremModal.activate(lorem);
+      };
     });
 
 })(angular);
--- dist/v-modal.js
+++ dist/v-modal.js
@@ -1,6 +1,6 @@
 /**
  * vModal - Simple, flexible and beautiful modal dialogs in AngularJS
- * @version v1.2.0
+ * @version v1.3.0
  * @link http://lukaszwatroba.github.io/v-modal
  * @author Łukasz Wątroba <l@lukaszwatroba.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -131,101 +131,103 @@ function vModalDirective () {
 }
 
 
- /*
-  * @license
-  * angular-modal v0.5.0
-  * (c) 2013 Brian Ford http://briantford.com
-  * License: MIT
-  */
+/*
+* @license
+* angular-modal v0.5.0
+* (c) 2013 Brian Ford http://briantford.com
+* License: MIT
+*/
 
 
- // vModal service
- angular.module('vModal.services')
-  .factory('vModal', vModalFactory);
+// vModal service
+angular.module('vModal.services')
+.factory('vModal', vModalFactory);
 
- function vModalFactory ($animate, $compile, $rootScope, $controller, $q, $http, $templateCache, $document, modalConfig) {
-   return function modalFactory (config) {
-     if (!(!config.template ^ !config.templateUrl)) {
-       throw new Error('Expected modal to have exacly one of either `template` or `templateUrl`');
-     }
+function vModalFactory ($animate, $compile, $rootScope, $controller, $q, $http, $templateCache, $document, modalConfig) {
+  return function modalFactory (config) {
+    if (!(!config.template ^ !config.templateUrl)) {
+      throw new Error('Expected modal to have exacly one of either `template` or `templateUrl`');
+    }
 
-     var controller    = config.controller || null,
-         controllerAs  = config.controllerAs,
-         container     = angular.element(config.container || $document[0].querySelector(modalConfig.containerSelector)),
-         element       = null,
-         html,
-         scope;
+    var controller    = config.controller || null,
+    controllerAs  = config.controllerAs,
+    container     = angular.element(config.container || $document[0].querySelector(modalConfig.containerSelector)),
+    element       = null,
+    html,
+    scope;
 
-     if (config.template) {
-       html = $q.when(config.template);
-     } else {
-       html = $http.get(config.templateUrl, {
-         cache: $templateCache
-       }).
-       then(function (response) {
-         return response.data;
-       });
-     }
+    if (config.template) {
+      html = $q.when(config.template);
+    } else {
+      html = $http.get(config.templateUrl, {
+        cache: $templateCache
+      }).
+      then(function (response) {
+        return response.data;
+      });
+    }
 
-     function activate (locals) {
-       return html.then(function (html) {
-         if (!element) {
-           attach(html, locals);
-         }
-       });
-     }
+    function activate (locals) {
+      return html.then(function (html) {
+        if (!element) {
+          attach(html, locals);
+        }
+      });
+    }
 
 
-     function attach (html, locals) {
-       element = angular.element(html);
-       if (element.length === 0) {
-         throw new Error('The template contains no elements; you need to wrap text nodes');
-       }
-       scope = $rootScope.$new();
-       if (controller) {
-         if (!locals) {
-           locals = {};
-         }
-         locals.$scope = scope;
-         var ctrl = $controller(controller, locals);
-         if (controllerAs) {
-           scope[controllerAs] = ctrl;
-         }
-       } else if (locals) {
-         for (var prop in locals) {
-           scope[prop] = locals[prop];
-         }
-       }
-       $compile(element)(scope);
-       container.attr('v-modal-open', '');
-       return $animate.enter(element, container);
-     }
+    function attach (html, locals) {
+      element = angular.element(html);
+      if (element.length === 0) {
+        throw new Error('The template contains no elements; you need to wrap text nodes');
+      }
+      scope = $rootScope.$new();
+      if (controller) {
+        if (!locals) {
+          locals = {};
+        }
+        for (var prop in locals) {
+          scope[prop] = locals[prop];
+        }
+        var ctrl = $controller(controller, {$scope: scope});
+        if (controllerAs) {
+          scope[controllerAs] = ctrl;
+        }
+      } else if (locals) {
+        for (var prop in locals) {
+          scope[prop] = locals[prop];
+        }
+      }
+      $compile(element)(scope);
+      container.attr('v-modal-open', '');
+      return $animate.enter(element, container);
+    }
 
-     function deactivate () {
-       if (!element) {
-         return $q.when();
-       }
-       return $animate.leave(element).then(function () {
-         scope.$destroy();
-         scope = null;
-         element.remove();
-         element = null;
-         container.removeAttr('v-modal-open');
-       });
-     }
+    function deactivate () {
+      if (!element) {
+        return $q.when();
+      }
+      return $animate.leave(element).then(function () {
+        scope.$destroy();
+        scope = null;
+        element.remove();
+        element = null;
+        container.removeAttr('v-modal-open');
+      });
+    }
 
-     function active () {
-       return !!element;
-     }
+    function active () {
+      return !!element;
+    }
 
-     return {
-       activate: activate,
-       deactivate: deactivate,
-       active: active
-     };
-   };
- }
+    return {
+      activate: activate,
+      deactivate: deactivate,
+      active: active
+    };
+  };
+}
 
- vModalFactory.$inject = ['$animate', '$compile', '$rootScope', '$controller', '$q', '$http', '$templateCache', '$document', 'modalConfig'];
+vModalFactory.$inject = ['$animate', '$compile', '$rootScope', '$controller', '$q', '$http', '$templateCache', '$document', 'modalConfig'];
 
 }(angular));
